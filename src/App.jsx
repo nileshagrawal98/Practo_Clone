52a511df18b428e2002da224ef4fad4471030b60
harishreyya <harishrebel0@gmail.com>
2022-01-23 03:20:38 +0530

    address , payment and order done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css';
 import {Lab} from "./components/Lab.jsx"
+import {Address} from "./components/address.jsx";
+import {Ordersuccess} from "./components/ordersuccess.jsx";
 
 function App() {
   return (
     <div className="App">
-     <Lab/>
      
+     <Ordersuccess/>
     </div>
   );
 }
