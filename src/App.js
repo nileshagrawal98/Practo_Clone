aa8ae845476054a8336ea6aa5070f8636d7653a2
nileshagrawal98 <nilesh_fw12_098@masai.school>
2022-01-22 20:28:07 +0530

    HomePage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { HomePage } from './Components/HomePage/HomePage';
+import { Navigation } from './Components/Navigation';
+import { Footer } from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <HomePage />
+      <Footer />
     </div>
   );
 }
