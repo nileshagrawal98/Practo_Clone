26792490f123bff2c2e30188d8ac5eb6229e84d0
SURAJ KUMAR <sksurajkumar7090@gmail.com>
2022-01-23 00:40:44 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+
+
+import { Route, Routes } from 'react-router-dom';
+import { Medicines } from './components/Medicines';
+import { Acne } from './components/Acne';
+import { Body } from './components/Body';
+
+import { ProductsDetails } from "./components/ProductsDetails"
+import { Cart } from './components/Cart';
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <Routes>
+
+        <Route path="/" element={<Medicines />}></Route>
+
+        <Route path="/acne" element={<Acne />}></Route>
+
+        <Route path="/body" element={<Body />}></Route>
+
+        <Route path="/:type/:userid" element={<ProductsDetails />}></Route>
+
+        <Route path="/cart" element={<Cart />}></Route>
+
+
+
+
+        <Route path="*" element={<div>404 page not found</div>}></Route>
+
+
+
+      </Routes>
+
+
     </div>
   );
 }
 
 export default App;
+
